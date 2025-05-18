4436346b98bfe6346a042301ab03b992f9e47b58
mohamedac29 <73788860+mohamedac29@users.noreply.github.com>
2025-05-18 10:06:30 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -226,27 +226,3 @@ def get_confusion_matrix(label, pred, size, num_class, ignore=-1):
                 confusion_matrix[i_label, i_pred] = label_count[cur_index]
     return confusion_matrix
 
-def adjust_learning_rate(optimizer, base_lr, max_iters, 
-        cur_iters, power=0.9, nbb_mult=10):
-    """
-    Adjust learning rate according to polynomial decay schedule.
-    Args:
-        optimizer: the optimizer whose learning rate should be changed
-        base_lr: initial learning rate
-        max_iters: maximum number of iterations
-        cur_iters: current iteration number
-        power: exponent for polynomial decay
-        nbb_mult: multiplier for bias parameters' learning rate
-    Returns:
-        The new learning rate
-    """
-    # Calculate new learning rate
-    lr = base_lr * ((1 - float(cur_iters) / max_iters) ** (power))
-    
-    # Update learning rate for main parameters
-    optimizer.param_groups[0]['lr'] = lr
-    
-    # Update learning rate for bias parameters if they're in a separate group
-    if len(optimizer.param_groups) == 2:
-        optimizer.param_groups[1]['lr'] = lr * nbb_mult
-    return lr
